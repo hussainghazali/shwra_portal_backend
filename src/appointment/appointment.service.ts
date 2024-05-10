5ff67268a10b2af8009bfdac548f62d331d7ebf9
hussainghazali <hussainghazali@gmail.com>
2024-05-10 10:10:07 +0500

    Added Update Appointment API

--- src/appointment/appointment.service.ts
+++ src/appointment/appointment.service.ts
@@ -4,6 +4,7 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Appointment } from "./entities/appointment.entity";
 import { RegisterUserDto } from "src/users/dto/register-user.dto";
 import { RegisterAppointmentDto } from "./dto/register-appointment.dto";
+import { UpdateAppointmentDto } from "./dto/update-appointment.dto";
 
 @Injectable()
 export class AppointmentService {
@@ -44,4 +45,35 @@ export class AppointmentService {
         };
       }
 
+      async update(updateAppointmentDto: UpdateAppointmentDto, id: string) {
+
+        const { industryType, legalIssue, meetingType, meetingSlot } = updateAppointmentDto;
+
+        const appointment = await this.appointmentRepository.findOne({ where: { id } });
+        if (!appointment) {
+          return {
+            message: 'No Appointment found',
+            data: []
+          };
+        }
+      
+        // Update the properties of the fetched shift entity
+        appointment.industryType = industryType;
+        appointment.legalIssue = legalIssue;
+        appointment.meetingType = meetingType;
+        appointment.meetingSlot = meetingSlot;
+      
+        try {
+          const updatedAppointment = await this.appointmentRepository.save(appointment);
+          return {
+            appointment: updatedAppointment,
+          };
+        } catch (error) {
+          return {
+            message: 'No appointment found',
+            data: [],
+            error
+          };
+        }
+      }
 }
